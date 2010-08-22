e0e7b5ce44adc563ce46eaa1100909e1e8feecce
andypotanin <andypotanin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-22 22:14:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-invoice/trunk@279412 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ core/ui/paypal.php
@@ -0,0 +1,139 @@
+
+<form action="https://www.paypal.com/us/cgi-bin/webscr" method="post" class="clearfix">
+
+	<input type="hidden" name="business" value="<?php echo $invoice->paypal_address; ?>">
+	<input type="hidden" name="no_shipping" value="1">
+	<input type="hidden" name="currency_code" value="<?php echo $invoice->currency_code; ?>">
+
+
+	
+<?php if($invoice->tax_total == 0) : ?>
+	<input type="hidden" name="tax"  value="<?php echo $invoice->tax_total; ?>">
+<?php endif; ?>
+	
+<?php if($invoice->is_recurring): ?>
+	<input type="hidden" name="return"  value="<?php echo $invoice->pay_link; ?>&paypal_recurring_return">
+	<input type="hidden" name="cmd" value="_xclick-subscriptions">
+	<input type="hidden" name="item_name" value="<?php echo ($invoice->subscription_name ? $invoice->subscription_name : $invoice->subject); ?>">
+	<input type="hidden" name="item_number" value="<?php echo $invoice->invoice_num; ?>">
+	<input type="hidden" name="cancel_return"  value="<?php echo $invoice->pay_link; ?>&return_info=cancel">
+	<input type="hidden" name="invoice" id="invoice_num"  value="<?php echo  $invoice->invoice_num; ?>">
+	<input type="hidden" name="srt"  value="<?php echo  $invoice->subscription_total_occurances; ?>">
+	<input type="hidden" name="src"  value="1">
+	
+	<?php
+		
+		switch ($invoice->subscription_unit) {
+			case 'days':
+				$subscription_unit = "D";
+			break;
+			case 'months':
+				$subscription_unit = "M";
+			break;
+			case 'weeks':
+				$subscription_unit = "W";
+			break;
+			case 'years':
+				$subscription_unit = "Y";
+			break;
+			
+		}
+	?>	
+	<input type="hidden" name="p3"  value="<?php echo $invoice->subscription_length; ?>">
+	<input type="hidden" name="t3"  value="<?php echo $subscription_unit; ?>">
+	<input type="hidden" name="a3"  value="<?php echo $invoice->amount; ?>">
+
+<?php endif; ?>
+
+<?php if(!$invoice->is_recurring): ?>
+	<input type="hidden" name="cmd" value="_ext-enter">
+	<input type="hidden" name="return"  value="<?php echo $invoice->pay_link; ?>&paypal_return">
+	<input type="hidden" name="rm" value="2">
+	<input type="hidden" name="upload" value="1">
+
+	<input type="hidden" name="notify_url"  value="<?php echo $invoice->pay_link; ?>">
+	
+
+	<input type="hidden" name="amount"  value="<?php echo $invoice->amount; ?>">
+	<input type="hidden" name="cbt"  value="Mark Invoice as Paid">
+	
+	<?php
+	// Convert Itemized List into PayPal Item List 
+	if(is_array($invoice->itemized)) echo wp_invoice_create_paypal_itemized_list($invoice->itemized,$invoice_id);
+	?>
+
+
+
+<?php endif; ?>
+
+
+
+	<fieldset id="credit_card_information">
+	<ol>		
+
+		<li>
+		<label for="first_name"><?php _e('First Name', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_inputfield("first_name",$invoice->user_class->first_name); ?>
+		</li>
+
+		<li>
+		<label for="last_name"><?php _e('Last Name', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_inputfield("last_name",$invoice->user_class->last_name); ?>
+		</li>
+
+		<li>
+		<label for="email"><?php _e('Email Address', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_inputfield("email_address",$invoice->user_class->user_email); ?>
+		</li>
+
+	<?php
+		list($day_phone_a, $day_phone_b, $day_phone_c) = split('[/.-]', $invoice->user_class->phonenumber);
+		?>
+		<li>
+		<label for="day_phone_a"><?php _e('Phone Number', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_inputfield("night_phone_a",$day_phone_a,' style="width:25px;" size="3" maxlength="3" '); ?>
+		<?php echo wp_invoice_draw_inputfield("night_phone_b",$day_phone_b,' style="width:25px;" size="3" maxlength="3" '); ?>
+		<?php echo wp_invoice_draw_inputfield("night_phone_c",$day_phone_c,' style="width:35px;" size="4" maxlength="4" '); ?>
+		</li>
+
+		<li>
+		<label for="address"><?php _e('Address', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_inputfield("address1",$invoice->user_class->streetaddress); ?>
+		</li>
+
+		<li>
+		<label for="city"><?php _e('City', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_inputfield("city",$invoice->user_class->city); ?>
+		</li>
+
+		<?php if(get_option('wp_invoice_fe_state_selection') != 'Hide') { ?>
+		<li id="state_field">
+		<label for="state"><?php _e('State', WP_INVOICE_TRANS_DOMAIN); ?></label>
+	<?php if(get_option('wp_invoice_fe_state_selection') == 'Dropdown') { ?>
+		<?php print wp_invoice_draw_select('state',wp_invoice_state_array(),$invoice->user_class->state);  ?>
+	<?php } ?>
+	<?php if(get_option('wp_invoice_fe_state_selection') == 'Input_Field') { ?>
+		<?php echo wp_invoice_draw_inputfield("state",$invoice->user_class->state); ?>
+	<?php } ?>
+		</li>
+		<?php } ?>
+
+		<li>
+		<label for="zip"><?php echo ((get_option('wp_invoice_custom_zip_label') != '') ? get_option('wp_invoice_custom_zip_label') : 'Zip Code'); ?></label>
+		<?php echo wp_invoice_draw_inputfield("zip",$invoice->user_class->zip); ?>
+		</li>
+
+		<li>
+		<label for="country"><?php _e('Country', WP_INVOICE_TRANS_DOMAIN); ?></label>
+		<?php echo wp_invoice_draw_select('country',wp_invoice_country_array(),$invoice->user_class->country); ?>
+		</li>
+
+		<li>
+		<label for="submit">&nbsp;</label>
+		<input type="image"  src="<?php echo get_option('wp_invoice_fe_paypal_link_url'); ?>" style="border:0; padding:0;" name="submit" alt="Make payments with PayPal - it's fast, free and secure!">
+		</li>
+		
+		<br class="cb" />	
+		</ol>
+	</fieldset>
+</form>
---
+++ core/ui/ui_overview_page.php
@@ -0,0 +1,127 @@
+<div class="wrap">
+<?php
+
+	// The error takes precedence over others being that nothing can be done w/o tables
+	if(!$wpdb->query("SHOW TABLES LIKE '".WP_Invoice::tablename('main')."';") || !$wpdb->query("SHOW TABLES LIKE '".WP_Invoice::tablename('log')."';")) { $warning_message = ""; }
+	
+ 
+	// Need to filter out recurring invoices
+	$all_invoices = $wpdb->get_results("SELECT * FROM ".WP_Invoice::tablename('main')." WHERE invoice_num != ''");
+
+ 
+?>		 
+	
+	<script>
+	 pagenow = 'toplevel_page_web-invoice_page_overview';
+	</script>
+	<form id="invoices-filter" action="" method="post" >
+	<h2><?php _e('Invoice Overview', WP_INVOICE_TRANS_DOMAIN); ?>
+	<a class="button add-new-h2" href="<?php echo admin_url("admin.php?page=new_invoice"); ?>">Add New</a>
+	</h2>
+
+	<div id="wpi_js_conflict_check" class="error">
+		<p>If you can see this message, there is a JavaScript conflict with one of the other plugins.  Please disable your plugins one-by-one to determine which plugin is causing the issue.  Once you find the problem, please contact TwinCitiesTech.com and we will add code to remove the given plugin's code from WP-Invoice pages. Thank you.</p>
+	</div>
+
+	<div class="wp_invoice_error_wrapper">
+	<?php if(is_array($errors) && count($errors) > 0): ?>
+	<div class="error"><p>
+		<?php foreach($errors as $error): ?>
+			<?php echo $error; ?><br />
+		<?php endforeach; ?>
+	</p></div>
+	<?php endif; ?>
+	<?php if(!is_array($errors) && !empty($errors)): ?>
+	<div class="error"><p>
+	<?php echo $errors; ?>
+	</p></div>
+	<?php endif; ?>
+	</div>
+
+	<?php if(count($messages) > 0): ?>
+	<div class="updated fade"><p>
+		<?php foreach($messages as $message): ?>
+			<?php echo $message; ?><br />
+		<?php endforeach; ?>
+	</p></div>
+	<?php endif; ?>
+	
+	
+	<div class="tablenav clearfix">
+	
+	<div class="alignleft">
+	<select id="wp_invoice_action" name="wp_invoice_action">
+		<option value="-1" selected="selected"><?php _e('-- Actions --', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="send_invoice" name="sendit" ><?php _e('Send Invoice(s)', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="send_reminder" name="sendit" ><?php _e('Send Reminder(s)', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="archive_invoice" name="archive" ><?php _e('Archive Invoice(s)', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="unrachive_invoice" name="unarchive" ><?php _e('Un-Archive Invoice(s)', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="mark_as_sent" name="mark_as_sent" ><?php _e('Mark as Sent', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="mark_as_paid" name="mark_as_paid" ><?php _e('Mark as Paid', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option value="mark_as_unpaid" name="mark_as_unpaid" ><?php _e('Unset Paid Status', WP_INVOICE_TRANS_DOMAIN); ?></option>
+		<option  value="delete_invoice" name="deleteit" ><?php _e('Delete', WP_INVOICE_TRANS_DOMAIN); ?></option>
+	</select>
+	<input type="submit" value="Apply" id="submit_bulk_action" class="button-secondary action" />
+	</div>
+
+	<div class="alignright">
+		<ul class="subsubsub" style="margin:0;">
+		<li><?php _e('Filter:', WP_INVOICE_TRANS_DOMAIN); ?></li>
+		<li><a href='#' class="" id="">All Invoices</a> |</li>
+		<li><a href='#'  class="paid" id="">Paid</a> |</li>
+		<li><a href='#'  class="sent" id="">Unpaid</a> |</li>
+		<li><?php _e('Custom: ', WP_INVOICE_TRANS_DOMAIN); ?><input type="text" id="FilterTextBox" class="search-input" name="FilterTextBox" /> </li>
+		</ul>
+	</div>
+	</div>
+	<br class="clear" />
+	
+	<table class="widefat fixed" cellspacing="0"  id="invoice_sorter_table">
+	<thead>
+	<tr class="thead">
+	<?php print_column_headers("toplevel_page_web-invoice_page_overview") ?>
+	</tr>
+	</thead>
+
+	<tfoot>
+	<tr class="thead">
+	<?php print_column_headers("toplevel_page_web-invoice_page_overview", false) ?>
+	</tr>
+	</tfoot>
+
+	<tbody id="invoices" class="list:invoices invoice-list">
+	<?php
+	$style = '';
+	$x_counter = 0;
+	$archived_count = 0;
+	foreach ($all_invoices as $invoice) {	
+
+		//$invoice = new WP_Invoice_GetInfo($invoice_id);
+		$invoice_class = new wp_invoice_get($invoice->invoice_num);
+		$invoice = $invoice_class->data;
+		
+		if($invoice->is_recurring)
+			continue;		
+			
+		if($invoice->is_archived)
+			$archived_count++;
+			
+		$style = ( ' class="alternate"' == $style ) ? '' : ' class="alternate"';
+		echo "\n\t" . wp_invoice_invoice_row($invoice);
+		$x_counter++;
+	}
+	
+	if($x_counter == 0) { ?>
+	<tr><td colspan="00" align="center"><div style="padding: 20px;"><?php _e('You have not created any invoices yet, ', WP_INVOICE_TRANS_DOMAIN); ?><a href="admin.php?page=new_invoice"><?php _e('create one now.', WP_INVOICE_TRANS_DOMAIN); ?></a></div></td></tr>
+	<?php }	?>
+	
+	</tbody>
+	</table>
+	</form> 
+
+	<?php 
+	if($archived_count++) { ?>
+		<a href="" id="wp_invoice_show_archived">Show / Hide Archived</a>
+	<?php }	
+	wp_invoice_overview_page_footer(); ?>
+</div>
