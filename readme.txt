72b35c7283de7eff93205df3b6d29b56df6fec51
andypotanin <andypotanin@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-28 15:19:43 +0000

    = Version 3.09.3 = * Fixed critical issues. * Fixed Stripe gateway's conflict. * Fixed fatal error on sending notifications. * Updated German (DE) localization. * Updated Russian (RU) localization.
    git-svn-id: https://plugins.svn.wordpress.org/wp-invoice/trunk@866993 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,9 +1,9 @@
 === WP-Invoice - Web Invoice and Billing ===
-Contributors: usability_dynamics, Anton Korotkoff, andypotanin, jbrw
+Contributors: usability_dynamics, anton-korotkoff, andypotanin, jbrw1984, maxim.peshkov, ideric
 Donate link: https://usabilitydynamics.com/products/wp-invoice/
 Tags: web invoice, bill, paypal, invoice, pay, online payment, send invoice, bill clients, authorize.net, credit cards, recurring billing, ARB, stripe, paypal, interkassa, google checkout, merchantplus
 Requires at least: 3.6
-Tested up to: 3.8.1
+Tested up to: 3.7
 Stable tag: 3.09.2
 WP-Invoice lets you create and send web-invoices and setup recurring billing for your clients.
 
@@ -104,6 +104,13 @@ Please visit the [wp-invoice community page](https://usabilitydynamics.com/produ
 * New features.
 
 == Change Log ==
+
+= Version 3.09.3 =
+* Fixed critical issues.
+* Fixed Stripe gateway's conflict.
+* Fixed fatal error on sending notifications.
+* Updated German (DE) localization.
+* Updated Russian (RU) localization.
 
 = Version 3.09.2 =
 * Fixed simple style issues.
--- wp-invoice.php
+++ wp-invoice.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://usabilitydynamics.com/products/wp-invoice/
  * Description: Send itemized web-invoices directly to your clients.  Credit card payments may be accepted via Authorize.net, MerchantPlus NaviGate, or PayPal account. Recurring billing is also available via Authorize.net's ARB. Visit <a href="admin.php?page=wpi_page_settings">WP-Invoice Settings Page</a> to setup.
  * Author: UsabilityDynamics.com
- * Version: 3.09.2
+ * Version: 3.09.3
  * Author URI: http://UsabilityDynamics.com/
  * Copyright 2011 - 2012  Usability Dynamics, Inc. (email : info@UsabilityDynamics.com)
  *
@@ -24,7 +24,7 @@
  */
 
 /* Define WPI Version */
-define( 'WP_INVOICE_VERSION_NUM', '3.09.2' );
+define( 'WP_INVOICE_VERSION_NUM', '3.09.3' );
 
 /* Define shorthand for transdomain */
 define( 'WPI', 'wp-invoice' );
@@ -62,6 +62,7 @@ require_once( WPI_Path . '/core/wpi_payment_api.php' );
 require_once( WPI_Path . '/core/ui/wpi_metaboxes.php' );
 require_once( WPI_Path . '/core/wpi_xmlrpc_api.php' );
 require_once( WPI_Path . '/core/wpi_dashboard_widget.php' );
+require_once( WPI_Path . '/core/ud_api.php' );
 
 //** Need to do this before init. Temporary here. */
 add_filter( "pre_update_option_wpi_options", array( 'WPI_Functions', 'pre_update_option_wpi_options' ), 10, 3 );
